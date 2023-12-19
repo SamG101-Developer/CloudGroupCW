0bb8abe5ce201ebccab37a6d5c5e7bdca1430b96
BenBurbridgeSoton <126475633+BenBurbridgeSoton@users.noreply.github.com>
2023-12-19 19:53:59 +0000

    Completed player_add
    + Adds a player to the database if they have a valid username and password.
    + If the request is successful, a validation response is sent to the client.
    + If an error occurs, the correct response is sent back to the client.

--- vpq/functions/player/player_add.py
+++ vpq/functions/player/player_add.py
@@ -1,9 +1,60 @@
+import json
+
 import azure.functions as func
 import logging
+import os
+from azure.cosmos import CosmosClient
+from azure.cosmos.exceptions import CosmosHttpResponseError
+from vpq.helper.player import Player, UsernameLengthError, PasswordLengthError, DatabaseContainsUsernameError
 
 function = func.Blueprint()
+
+cosmos = CosmosClient.from_connection_string(os.environ['AzureCosmosDBConnectionString'])
+database = cosmos.get_database_client(os.environ['DatabaseName'])
+playerContainer = database.get_container_client(os.environ['Players'])
 
 
 @function.route(route="playerAdd", auth_level=func.AuthLevel.ANONYMOUS, methods=["POST"])
 def playerAdd(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    try:
+        reqJson = req.get_json()
+        logging.info('Python HTTP trigger function processed a request to add a player. JSON: {}'.format(reqJson))
+
+        # Check the username and password are valid
+        player = Player(reqJson)
+        player.isUsernameValid()
+        player.isPasswordValid()
+
+        # Check the database does NOT contain the username
+        query = "SELECT * FROM p where p.username='{}'".format(reqJson['username'])
+        usernameExists = len(list(playerContainer.query_items(query=query, enable_cross_partition_query=True))) != 0
+        if usernameExists:
+            raise DatabaseContainsUsernameError
+
+        # Add the player to the database
+        playerContainer.create_item(body=reqJson, enable_automatic_id_generation=True)
+
+        logging.info("User Added Successfully")
+        return func.HttpResponse(body=json.dumps({'result': True, "msg": "Success"}), mimetype="application/json")
+
+    except UsernameLengthError:
+        logging.error("Username length invalid.")
+        return func.HttpResponse(body=json.dumps({'result': False, "msg": "Username length invalid"}),
+                                 mimetype="application/json")
+
+    except PasswordLengthError:
+        logging.error("Password length invalid.")
+        return func.HttpResponse(body=json.dumps({'result': False, "msg": "Password length invalid"}),
+                                 mimetype="application/json")
+
+    except DatabaseContainsUsernameError:
+        logging.error("Database already contains username.")
+        return func.HttpResponse(body=json.dumps({'result': False, "msg": "Database already contains username"}),
+                                 mimetype="application/json")
+
+    except CosmosHttpResponseError:
+        logging.error("Did not complete the request due to an issue connecting to the database."
+                      " Please try again later.")
+        return func.HttpResponse(body=json.dumps({'result': False, "msg": "Database already contains username"}),
+                                 mimetype="application/json")
+
