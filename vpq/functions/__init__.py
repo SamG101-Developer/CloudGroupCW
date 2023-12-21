b03ec47bc8d8a0d31a0b10d478ba433b7a633c7b
Sam <samuelgardner101@gmail.com>
2023-12-21 00:28:53 +0000

    update skeleton functions

--- vpq/functions/__init__.py
+++ vpq/functions/__init__.py
@@ -1,5 +1,6 @@
 import vpq.functions.player as player
 import vpq.functions.question as question
+import vpq.functions.question_set as question_set
 import vpq.functions.room as room
 
-functions = player.player_functions + question.question_functions + room.room_functions
+functions = player.player_functions + question.question_functions + question_set.question_set_functions + room.room_functions
