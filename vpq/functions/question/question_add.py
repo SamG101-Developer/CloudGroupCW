11c96b5045c3634e75d4943971b2176c6c64f3b2
Sam <samuelgardner101@gmail.com>
2023-12-22 15:30:38 +0000

    error checking

--- vpq/functions/question/question_add.py
+++ vpq/functions/question/question_add.py
@@ -6,12 +6,13 @@ import azure.functions as func
 from azure.cosmos import CosmosClient
 from azure.cosmos.exceptions import CosmosHttpResponseError
 
-from vpq.helper.exceptions import CosmosHttpResponseErrorMessage
+from vpq.helper.exceptions import CosmosHttpResponseErrorMessage, DatabaseDoesNotContainUsernameError
 from vpq.helper.question import Question, QuestionLengthError
 
 function = func.Blueprint()
 cosmos = CosmosClient.from_connection_string(os.environ['AzureCosmosDBConnectionString'])
 database = cosmos.get_database_client(os.environ['DatabaseName'])
+playerContainer = database.get_container_client(os.environ['Container_Players'])
 questionContainer = database.get_container_client(os.environ['Container_Questions'])
 
 
@@ -25,6 +26,12 @@ def questionAdd(req: func.HttpRequest) -> func.HttpResponse:
         question = Question(reqJson)
         question.isQuestionValid()
 
+        # Check the author exists
+        author_query = "SELECT p.username FROM p where p.username='{}'".format(reqJson['author'])
+        authors = list(playerContainer.query_items(query=author_query, enable_cross_partition_query=True))
+        if len(authors) == 0:
+            raise DatabaseDoesNotContainUsernameError
+
         # Add the question to the database
         questionContainer.create_item(body=reqJson, enable_automatic_id_generation=True)
         logging.info("Question Added Successfully")
@@ -32,6 +39,11 @@ def questionAdd(req: func.HttpRequest) -> func.HttpResponse:
 
     except QuestionLengthError:
         message = QuestionLengthError.getMessage()
+        logging.error(message)
+        return func.HttpResponse(body=json.dumps({'result': False, "msg": message}), mimetype="application/json")
+
+    except DatabaseDoesNotContainUsernameError:
+        message = DatabaseDoesNotContainUsernameError.getMessage()
         logging.error(message)
         return func.HttpResponse(body=json.dumps({'result': False, "msg": message}), mimetype="application/json")
 
