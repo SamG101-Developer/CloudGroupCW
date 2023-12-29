3f229296c4659b5183bfae46c8ccd15f7982aa28
EthanTravers <et2g21@soton.ac.uk>
2023-12-29 20:30:48 +0000

    -

--- vpq/functions/room/room_admin_set.py
+++ vpq/functions/room/room_admin_set.py
@@ -3,6 +3,9 @@ import logging
 
 function = func.Blueprint()
 
+# sets the admin of the room to a player's username.
+# this is the player that creates the lobby (or can become admin if the current admin leaves).
+# they will start the game, advance the stages etc
 
 @function.route(route="roomAdminSet", auth_level=func.AuthLevel.ANONYMOUS)
 def roomAdminSet(req: func.HttpRequest) -> func.HttpResponse:
