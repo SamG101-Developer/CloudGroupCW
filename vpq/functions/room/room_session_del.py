175acbcca2f8645ba6f8a7f5258d4a316730c10c
prav6 <praveenchandrarajah@gmail.com>
2024-01-05 14:36:26 +0000

    room session delete again

--- vpq/functions/room/room_session_del.py
+++ vpq/functions/room/room_session_del.py
@@ -7,9 +7,12 @@ from azure.cosmos import CosmosClient
 from azure.cosmos.exceptions import CosmosHttpResponseError
 
 try:
-    from helper.exceptions import CosmosHttpResponseErrorMessage, RoomDoesNotExist
+    from helper.exceptions import CosmosHttpResponseErrorMessage
+    from helper.room import Room, RoomDoesNotExist
+
 except ModuleNotFoundError:
-    from vpq.helper.exceptions import CosmosHttpResponseErrorMessage, RoomDoesNotExist
+    from vpq.helper.exceptions import CosmosHttpResponseErrorMessage
+    from vpq.helper.room import Room, RoomDoesNotExist
 
 function = func.Blueprint()
 
@@ -21,11 +24,12 @@ def roomSessionDel(req: func.HttpRequest) -> func.HttpResponse:
         roomContainer = database.get_container_client(os.environ['Container_Rooms'])
 
         reqJson = req.get_json()
-        adminUsername = reqJson["adminUsername"]
-        logging.info(f"Python HTTP trigger function processed a request to delete a room: Admin Username: {adminUsername}.")
 
-        # Query to find the room by admin username
-        query = "SELECT r.id FROM r WHERE r.room_admin='{}'".format(adminUsername)
+        username = reqJson["username"]
+        logging.info(f"Python HTTP trigger function processed a request to delete a room: Admin Username: {username}.")
+
+        # Query to find the room by admin username (room_admin)
+        query = "SELECT * FROM r where r.room_admin='{}'".format(username)
         rooms = list(roomContainer.query_items(query=query, enable_cross_partition_query=True))
         if len(rooms) == 0:
             raise RoomDoesNotExist
