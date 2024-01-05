fa6f3d5104f5290d87b57321173fb5b77f45540e
prav6 <praveenchandrarajah@gmail.com>
2024-01-05 21:12:53 +0000

    working changes

--- vpq/functions/room/room_flag_password_set.py
+++ vpq/functions/room/room_flag_password_set.py
@@ -3,7 +3,23 @@ import logging
 
 function = func.Blueprint()
 
+from vpq.helper.player import Player,PasswordLengthError
 
 @function.route(route="roomFlagPasswordSet", auth_level=func.AuthLevel.ANONYMOUS)
 def roomFlagPasswordSet(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+    try:
+        #lock the room with a password to the room
+
+        reqJson = req.get_json()
+        adminUsername = reqJson['adminUsername']
+        password = reqJson['valueToSet']
+
+
+        passwordCheck = Player({'password':password})
+        passwordCheck.isPasswordValid()
+
+
+    except PasswordLengthError:
+        message = PasswordLengthError.getMessage()
+        return func.HttpResponse(body=json.dumps({'result': False, "msg": message}),mimetype="application/json")
--- vpq/functions/room/room_player_del.py
+++ vpq/functions/room/room_player_del.py
@@ -1,9 +1,83 @@
-import azure.functions as func
+import json
 import logging
+import os
+
+import azure.functions as func
+from azure.cosmos import CosmosClient
+from azure.cosmos.exceptions import CosmosHttpResponseError
+
+try:
+    # Import custom exceptions
+    from helper.exceptions import CosmosHttpResponseErrorMessage
+    from helper.room import RoomDoesNotExist, UserDoesNotExist, UserNotInRoom
+except ModuleNotFoundError:
+    # Fallback imports for different module structure
+    from vpq.helper.exceptions import CosmosHttpResponseErrorMessage
+    from vpq.helper.room import RoomDoesNotExist, UserDoesNotExist, UserNotInRoom
 
 function = func.Blueprint()
 
 
-@function.route(route="roomPlayerDel", auth_level=func.AuthLevel.ANONYMOUS)
+@function.route(route="roomPlayerDel", auth_level=func.AuthLevel.ANONYMOUS, methods=["DELETE"])
 def roomPlayerDel(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    try:
+        # Set up Cosmos DB client
+        cosmos = CosmosClient.from_connection_string(os.environ['AzureCosmosDBConnectionString'])
+        database = cosmos.get_database_client(os.environ['DatabaseName'])
+        roomContainer = database.get_container_client(os.environ['Container_Rooms'])
+
+        # Parse request JSON for required data
+        reqJson = req.get_json()
+        adminUsername = reqJson['adminUsername']
+        usernameToRemove = reqJson['usernameToRemove']
+
+        # Query to find the room by the admin username
+        query = "SELECT * FROM r WHERE r.room_admin = '{}'".format(adminUsername)
+        rooms = list(roomContainer.query_items(query=query, enable_cross_partition_query=True))
+
+        # Check if room exists
+        if len(rooms) == 0:
+            raise RoomDoesNotExist
+
+        room = rooms[0]
+
+        #check to see if admin player is removed
+        if adminUsername == usernameToRemove:
+            return func.HttpResponse(body=json.dumps({'result': False, "msg": "Cannot remove admin user"}),mimetype="application/json")
+
+        # Check if user to remove is in the room
+        if usernameToRemove not in room['players_in_room']:
+            raise UserNotInRoom
+
+        # Remove the user from the room's player list
+        playersList = room['players_in_room']
+        roomId = room['id']
+        playersList.remove(usernameToRemove)
+
+        item_to_modify= roomContainer.read_item(item=roomId, partition_key=roomId)
+        item_to_modify['players_in_room'] = playersList
+        roomContainer.replace_item(item=item_to_modify,body= item_to_modify)
+
+        # Return success response
+        return func.HttpResponse(body=json.dumps({'result': True, "msg": "User removed from room."}),mimetype="application/json")
+
+    # Handle specific exceptions and return appropriate responses
+    except RoomDoesNotExist:
+        message = RoomDoesNotExist.getMessage()
+        logging.error(message)
+        return func.HttpResponse(body=json.dumps({'result':False, 'msg':message}), mimetype="application/json")
+
+    except UserDoesNotExist:
+        message = UserDoesNotExist.getMessage()
+        logging.error(message)
+        return func.HttpResponse(body=json.dumps({'result':False, 'msg':message}), mimetype="application/json")
+
+    except UserNotInRoom:
+        message = UserNotInRoom.getMessage()
+        logging.error(message)
+        return func.HttpResponse(body=json.dumps({'result':False, 'msg':message}), mimetype="application/json")
+
+    except CosmosHttpResponseError as e:
+        message = CosmosHttpResponseErrorMessage(e)
+        logging.error(message)
+        return func.HttpResponse(body=json.dumps({'result': False, "msg": message}), mimetype="application/json")
