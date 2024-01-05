82c526ed23df4f9cd9d63772adaab471a1a86c82
kagethan123 <kagethan123@gmail.com>
2024-01-05 15:09:32 +0000

    Changed the buttons to be an on click command

--- vpq/front-end/server.js
+++ vpq/front-end/server.js
@@ -33,7 +33,7 @@ console.log(BACKEND_ENDPOINT);
 function startServer() {
     const PORT = process.env.PORT || 8080;
     server.listen(PORT, () => {
-        console.log(`Server listening on port ${PORT}`);
+        console.log(`Server listening on port ${PORT}    http://localhost:8080/`);
     });
 }
 
